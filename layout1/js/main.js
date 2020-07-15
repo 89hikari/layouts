7a2432c61f634ed051f93f62074ebffaddfd0e71
89hikari <vladislav0151@bk.ru>
2020-07-15 21:08:01 +0300

    header done

---
+++ layout1/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next">Next</button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev">prev</button>',
+    });
+});
