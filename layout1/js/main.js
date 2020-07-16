47d910494f9f53a5cd51f3604f7fb440028a56e2
89hikari <vladislav0151@bk.ru>
2020-07-16 11:45:03 +0300

    slider done

--- layout1/js/main.js
+++ layout1/js/main.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.slider-inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next">Next</button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev">prev</button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
     });
 });
